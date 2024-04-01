73f1da7c8d32efe5cf7fccc583078c841b47168f
Thiago Mota <euthiagojose2004@gmail.com>
2024-04-01 18:08:23 -0300

    master

--- src/filmes/dto/create-filme.dto.ts
+++ src/filmes/dto/create-filme.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty } from "class-validator"
+
+
 export class CreateFilmeDto {
+
+    @IsNotEmpty({message: 'Por favor, digite o título do filme.'})
    title: string
+
+    @IsNotEmpty({message: 'Por favor, digite a descrição do filme.'})
     description: string
+
+    @IsNotEmpty({message: 'Por favor, digite o nome do autor do filme.'})
     author: string
 }
