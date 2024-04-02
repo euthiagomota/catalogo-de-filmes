9e41ed8252660cb390b9f958d813f97249145ffa
Thiago Mota <euthiagojose2004@gmail.com>
2024-04-02 16:25:05 -0300

    Fiz alterações no dto do projeto, nas validações.

--- src/filmes/dto/create-filme.dto.ts
+++ src/filmes/dto/create-filme.dto.ts
@@ -3,12 +3,12 @@ import { IsNotEmpty } from "class-validator"
 
 export class CreateFilmeDto {
 
-    @IsNotEmpty({message: 'Por favor, digite o título do filme.'})
+    @IsNotEmpty({message: 'Por favor, digite o título do filme para executar a função corretamente.'})
    title: string
 
-    @IsNotEmpty({message: 'Por favor, digite a descrição do filme.'})
+    @IsNotEmpty({message: 'Por favor, digite a descrição do filme para executar a função corretamente.'})
     description: string
 
-    @IsNotEmpty({message: 'Por favor, digite o nome do autor do filme.'})
+    @IsNotEmpty({message: 'Por favor, digite o nome do autor do filme para executar a função corretamente.'})
     author: string
 }
